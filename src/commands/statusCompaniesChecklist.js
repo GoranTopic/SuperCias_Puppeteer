9012ed0b55ef514035f70a4b06c193989a4e50cc
GoranTopic <telegonicaxx@live.com>
2023-02-08 14:45:15 -0500

    latest

--- src/commands/statusCompaniesChecklist.js
+++ src/commands/statusCompaniesChecklist.js
@@ -1,5 +1,7 @@
 import Checklist from '../progress/Checklist.js'
 import options from '../options.js'
+import DiskList from '../progress/DiskList.js'
+
 
 
 // get the data directory
@@ -12,7 +14,13 @@ let checklist = new Checklist(
 	{ recalc_on_check: false }
 );
 
+let errored = new DiskList(
+	'errored_companies', null,
+	data_directory + '/resources/list'
+);
+
 
 console.log('checked companies:     ', checklist.valuesDone())
 console.log('number of companies:   ', checklist.valuesCount())
 console.log('get missing companies: ', checklist.missingLeft())
+console.log('errored comanies:       ', errored.valuesCount())
