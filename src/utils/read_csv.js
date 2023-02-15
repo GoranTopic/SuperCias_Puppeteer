7a9fad6647151955529c5d56416824db710a15e5
GoranTopic <telegonicaxx@live.com>
2023-02-15 11:47:14 -0800

    added read csv file

---
+++ src/utils/read_csv.js
@@ -0,0 +1,15 @@
+import fs from 'fs'
+import csv from 'csv-parser';
+
+const read_csv = async csv_file => 
+    await new Promise( (res, rej) => {
+        const results = [];
+        fs.createReadStream(csv_file)
+            .pipe(csv())
+            .on('data', (data) => {
+                results.push(data);
+            })
+            .on('end', () => {
+                res(results);
+            });
+    });
