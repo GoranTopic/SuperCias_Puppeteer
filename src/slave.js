d06f802e65df1c8ace24f6f07784792507677cb5
Goran Topic <telegonicaxx@live.com>
2024-04-12 14:01:09 -0400

    added urls file

--- src/slave.js
+++ src/slave.js
@@ -3,8 +3,8 @@ import setup_browser from './scripts/setup_browser.js';
 import goto_page from './scripts/goto_page.js';
 import scrap_cedula_suggestion from './scripts/scrap_cedula_suggestion.js'
 import close_browser from './scripts/close_browser.js';
+import { search_page } from './urls.js';
 
-let url = 'https://appscvs1.supercias.gob.ec/consultaPersona/consulta_cia_param.zul'
 
 slavery({
     numberOfSlaves: 1,
---
+++ src/urls.js
@@ -0,0 +1,7 @@
+let domain = 'https://appscvs1.supercias.gob.ec/consultaPersona/';
+
+let search_page = domain + 'consulta_cia_param.zul';
+
+let consulta_personal = domain + 'consulta_cia_personas.zul';
+
+export { domain, search_page, consulta_personal }
