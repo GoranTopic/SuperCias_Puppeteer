a01a9eb7e5f821ff632e542a0f054fd9ff50fe05
Goran Topic <telegonicaxx@live.com>
2024-04-16 14:34:32 -0400

    data cleaning suggestion

---
+++ clean/.ipynb_checkpoints/clean-checkpoint.py
@@ -0,0 +1,10 @@
+# import pandas
+import pandas as pd
+
+# read file from csv and store it in a dataframe
+df_cedulas = pd.read_csv('../storage/Registro-Civil.cedulas.csv')
+
+print(df_cedulas.head())
+
+
+
