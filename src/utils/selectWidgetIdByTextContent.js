f94833785846a6f0c5bd577af0284bda3eef823f
Goran Topic <telegonicaxx@live.com>
2024-04-17 02:54:04 -0400

    deleted uneede files

--- src/utils/selectWidgetIdByTextContent.js
+++
@@ -1,9 +0,0 @@
-const selectWidgetIdByTextContent = async (page, textContent) =>
-    await page.evaluate((textContent) => {
-        const elements = Array.from(document.querySelectorAll('span'));
-        const selectedElement = elements.find((el) => el.textContent.trim() === textContent);
-        return selectedElement.parentNode.id.split(':').pop();
-    }, textContent);
-
-export default selectWidgetIdByTextContent;
-
