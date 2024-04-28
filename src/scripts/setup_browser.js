d531636ae5df6a15e44d17232a6ea320bc69ae4f
Goran Topic <telegonicaxx@live.com>
2024-04-28 13:49:21 -0400

    added password proxies

--- src/scripts/setup_browser.js
+++ src/scripts/setup_browser.js
@@ -2,7 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 import puppeteer from 'puppeteer';
 
-
 import options from '../options.js';
 // get eviroment variables
 const { username, password, proxyEndpoint } = process.env;
@@ -10,17 +9,19 @@ const { username, password, proxyEndpoint } = process.env;
 const setup_browser = async proxy => {
     // set new proxy, while keeping args
     if(proxy) options.browser.args = [
-        //`--proxy-server=${proxyEndpoint}`,
-        //`--proxy-auth=${username}:${password}`,
-        `--proxy-server=http://${ proxy }`,
+        `--proxy-server=${proxyEndpoint}`,
+        `--proxy-auth=${username}:${password}`,
+        //`--proxy-server=http://${ proxy }`,
         ...options.browser.args
     ];
 
     // create new browser
     const browser = await puppeteer.launch({ ...options.browser });
-    // Set the proxy credentials
-    //let page = await (await browser.pages())[0]
-        //.authenticate({ username, password });
+    // authenticate browser
+    console.log('Authenticating browser...');
+    let pages = await browser.pages();
+    await pages[0].authenticate({ username, password });
+    //await (await browser.pages())[0].authenticate({ username, password });
     // return browser
     return browser;
 }
