c2424290497b069d24a66e4f37d56908bd885d96
Goran Topic <telegonicaxx@live.com>
2024-04-30 19:49:03 -0400

    adding rever engeneer type

---
+++ src/scripts/menus_items/scrap_accionistas_tabla_opction.js
@@ -0,0 +1,81 @@
+import sanitize from '../../utils/sanitizer.js';
+import send_request from '../../reverse_engineer/send_request.js';
+import { query_table_change } from '../../reverse_engineer/queries/query_tables_change_general.js';
+import scrap_pdf_row from './scrap_documents_pdf_row.js';
+import waitForNetworkIdle from '../../utils/waitForNetworkIdle.js';
+import options from '../../options.js';
+
+
+/**
+ * scrap_table.
+ * this is the piece of the strip that script that handle scraping a table o rows
+ *
+ * @param {} table
+ * @param {} rows
+ * @param {} checklists
+ * @param {} page
+ * @param {} company
+ */
+const scrap_table = async (table, rows, checklists, page, company ) => {
+    // switch table tab let's change the tab and get the total number of rows, 
+    // except if it is the general row, in which case it is 
+    console.log(`scraping ${table} Table`);
+    
+    console.log(`rows[${table}]: ${rows[table]}`);
+
+    // add filters to the table
+    console.log('adding filters table')
+    await page.evaluate(({ table, filters }) => {
+        // get filters values
+        console.log(filters);
+    
+        const widget = PrimeFaces.widgets['tbl' + table];
+        if (widget && widget.sortableColumns) {
+            Object.values(filters).forEach((value, i) => {
+                if (widget.sortableColumns[i]) {
+                    widget.sortableColumns[i].childNodes[3].value = value;
+                }
+            });
+            widget.filter();
+        } else {
+            console.log('El widget o las columnas ordenables no están definidos correctamente.');
+        }
+    }, { table, filters: options.documents[table].filters });
+
+    // wait for table to load
+    await waitForNetworkIdle(page, 1000);
+
+    // don't try to scrap if the are no documents
+    if(rows[table] === 0) return true
+    // let make update the path 
+
+    if (rows[table] > 10) {
+        // get all rows
+        console.log('sending query for rows all')
+        await page.evaluate( ({ table, rows }) => { // paginator
+                return PrimeFaces
+                    .widgets['tbl' + table]
+                    .paginator
+                    .setRowsPerPage(rows)
+            }, { table, rows: rows[table] }
+        )
+    }
+
+    // wait for table to load
+    await waitForNetworkIdle(page, 1000);
+    let dataTable = options.documents[table].filters;
+    // extract rows in table
+    let pdfs_info = [];
+    pdfs_info = await page.evaluate( (table,dataTable) =>
+        // let get a list of all pdf documents
+        // note: here the value is tab + table
+        // instead of the ususal tbl + table
+        window.data_extract_table_html('tbl' + table, dataTable),
+        table,
+        dataTable
+    );
+
+    return pdfs_info;
+}
+
+export default scrap_table;
---
+++ src/scripts/menus_items/scrap_documents_pdf_row.js
@@ -0,0 +1,37 @@
+import { mkdir } from 'files-js';
+import download_pdf from '../../utils/download_pdf.js';
+import send_request from '../../reverse_engineer/send_request.js';
+import query_pdf from '../../reverse_engineer/queries/query_pdf_link.js';
+import options from '../../options.js';
+
+
+const scrap_row = async (id, page, path) =>
+    // set a time out for 4 minutes, to proces the pdf
+    await new Promise(async (resolve, reject) => {
+        let time_out = setTimeout(() => {
+            console.log('pdf request timed out');
+            reject(false);
+        }, 60 * 1000 * 4) // set timed out to 4 minutes
+        // requestin pdf link
+        let src = await send_request(
+            query_pdf(id),
+            (response, status, i, C) => {
+                // return the src of the pdf
+                return window.parse_pdf_src(response);
+            },
+            page,
+            console,
+            false, // don't followAlong so we don't download the pdf twice
+        );
+        console.log(`src: ${src}`);
+        // downloading pdf
+        let pdf_str = await download_pdf(
+            src, // where to download
+            page,// the page to download with 
+            path // where to save
+        );
+        clearTimeout(time_out);
+        resolve(pdf_str);
+    });
+
+export default scrap_row;
