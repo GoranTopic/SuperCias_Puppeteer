8af5ad88b72bdf734a4a04f3a31fb963a42e94e3
Goran Topic <telegonicaxx@live.com>
2024-07-28 11:04:47 -0400

    feat: Update folder path for ranking_bi.ipynb

--- src/init.js
+++ src/init.js
@@ -8,19 +8,22 @@ const init = async () => {
     let storage = new Storage({
         type: 'mongodb',
         url: 'mongodb://0.0.0.0:27017',
-        database: 'supercias',
+        database: 'supercias_ranking',
     });
     //    
     let fileStorage = new Storage({
         type: 'mongoFiles',
         url: 'mongodb://0.0.0.0:27017',
-        database: 'supercias',
+        database: 'supercias_ranking',
     });
     // open the store
-    let suggestions_store = await storage.open('companies_suggestions');
+    let suggestions_store = await storage.open('rankings');
     // get suggestions
-    let cedulas_to_scrap = await suggestions_store.get({});
-    console.log('cedulas to scrap', cedulas_to_scrap.length);
+    let rucs_to_scrap = await suggestions_store.get({
+        "activos": { $gt: 500000 }
+    })  
+    rucs_to_scrap = rucs_to_scrap.map( r => ({ "id": r.expediente, "ruc": r.ruc, "name": r.nombre }) );
+    //console.log('cedulas to scrap:', rucs_to_scrap.length);
     // close the store
     await suggestions_store.close();
     // Create a file
@@ -31,8 +34,8 @@ const init = async () => {
     mkdir('./storage/checklists');
     // Read the file
     let checklist = new Checklist(
-        cedulas_to_scrap, { 
-            name: 'companies_suggestions',
+        rucs_to_scrap, { 
+            name: 'companies_rucs',
             path: './storage/checklists',
             recalc_on_check: false,
             save_every_check: 1,
--- src/main.js
+++ src/main.js
@@ -11,16 +11,18 @@ console.log('gottten company', company);
 
 // set up browser
 let browser = await setup_browser();//proxy);
-// save fileStore in to the page
+// save fileStore in to the page so that it can be used in the browser
 (await browser.pages())[0]['fileStore'] = fileStore;
 // scrap company
-let data = await scrap_company(browser, company);
+//let data = await scrap_company(browser, company);
 // clean up
-await close_browser( browser );
+//await close_browser( browser );
 
-console.log(data);
+
+/*console.log(data);
 if (data) {
     await store.push(data);
     checklist.check(company);
     console.log('checked');
 }
+*/
