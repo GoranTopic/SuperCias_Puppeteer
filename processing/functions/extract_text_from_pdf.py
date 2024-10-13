69a8b2c23d5c70fb5f62b66da0c0c85bc4d69535
Goran Topic <telegonicaxx@live.com>
2024-10-13 19:26:43 -0400

    Refactor extract_text_from_pdf.py: Update text extraction from PDF using pytesseract

--- processing/functions/extract_text_from_pdf.py
+++ processing/functions/extract_text_from_pdf.py
@@ -1,42 +1,25 @@
-import fitz  # PyMuPDF
 import pytesseract
-from pdf2image import convert_from_path
-from PIL import Image
+from pdf2image import convert_from_path 
 import os
 import io
+
+os.environ['TESSDATA_PREFIX'] = '/usr/share/tesseract/tessdata'
 
 def extract_text_from_pdf(pdf_filename):
     # print current working direcotry
     # Open the PDF file
-    pdf_document = fitz.open( pdf_filename )
     text = ""
-    # Loop through each page in the PDF
-    for page_num in range(pdf_document.page_count):
-        page = pdf_document[page_num]
-        
-        # Extract images from the page
-        image_list = page.get_images(full=True)
-        
-        # If there are images, use OCR to extract text
-        if image_list:
-            for image_index, img in enumerate(page.get_images(full=True)):
-                xref = img[0]
-                base_image = pdf_document.extract_image(xref)
-                image_bytes = base_image["image"]
-
-                # Convert to a PIL Image
-                byteImageIO = io.BytesIO(image_bytes)
-                image = Image.open(byteImageIO)
-                
-                # Use pytesseract to do OCR on the image on spanish 
-                text += pytesseract.image_to_string(image, lang='spa')
-        else:
-            # If no images, extract text as usual
-            text += page.get_text()
-
+    images = convert_from_path(pdf_filename)
+    for i, image in enumerate(images):
+        # rotate if needed
+        osd = pytesseract.image_to_osd(image)
+        angle = int(osd.split('\n')[2].split(': ')[1])
+        image = image.rotate(angle, expand=True)
+        # use tessaract to extract text
+        text += pytesseract.image_to_string(image, lang='spa')
     return text
 
 # Example usage for testing purposes
-#pdf_filename = "./storage/pdfs/1790721450001_DocumentosGenerales_Oficio Nombramiento Administradores_2020-09-04_ALVERNIA DE CHACON IMELDA_PRESIDENTE.pdf"
-#text = extract_text_from_pdf(pdf_filename)
-#print(text)
+pdf_filename = "./storage/pdfs/1790721450001_DocumentosGenerales_Oficio Nombramiento Administradores_2020-09-04_ALVERNIA DE CHACON IMELDA_PRESIDENTE.pdf"
+text = extract_text_from_pdf(pdf_filename)
+print(text)
