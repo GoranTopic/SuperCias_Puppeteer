3e83d55876a6291671acb8b3dcb4e2865e9e16b0
GoranTopic <telegonicaxx@live.com>
2024-11-10 18:53:42 -0500

    added clean execels

---
+++ processing/kardek_de_acctionistas/clean_empty_excels.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+import gridfs
+
+# Connect to the local MongoDB instance
+client = MongoClient('mongodb://localhost:27017/')
+
+# Access the 'supercias' database
+db = client['supercias']
+
+# Access the 'kradek_de_accioniestas' collection
+collection = db['kradek_de_accioniestas']
+
+# Access the GridFS for 'kradek_de_accioniestas.files'
+fs = gridfs.GridFS(db, collection='kradek_de_accioniestas')
+
+# Get all files in the 'kradek_de_accioniestas.files' collection
+files_collection = db['kradek_de_accioniestas.files']
+
+# Iterate over each file in the collection
+for file in files_collection.find():
+    if file['length'] <= 1000:
+        file_id = file['_id']
+        try:
+            # Remove the file from the GridFS collection
+            fs.delete(file_id)
+            # Remove the corresponding document from the 'kradek_de_accioniestas' collection
+            collection.delete_one({'_id': file_id})
+            print(f"Deleted file with _id: {file_id}")
+        except gridfs.errors.NoFile:
+            print(f"No file found for _id: {file_id}")
+
+print("Cleanup complete.")
